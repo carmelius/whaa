ebca03eeaf95eb091ae7b83e0a790e2c7254cc3a
carmelius <carmelius.umbah@icloud.com>
2023-11-16 05:05:16 +0000

    fist commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <body>
+        <h1>welcome to jumanji</h1>
+      </body>
+    </html>
+  );
+}
+
+export default App;
